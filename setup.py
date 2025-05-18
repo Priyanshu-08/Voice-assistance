58da3693f4a058b88d2f8aecabe811c339ab4436
Priyanshu_XD <103308519+Priyanshu-08@users.noreply.github.com>
2025-05-18 14:17:05 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     author="Priyanshu",
     author_email="impriyanshusarkar@gmail.com",
     packages=find_packages(),
-    install_requires=["SpeechRecognition","pipwin","pyaudio","gTTS","google-generativeai","python-dotenv","streamlit"]
+    install_requires=["SpeechRecognition","pipwin","pyaudio","gTTS","google-generativeai","python-dotenv","streamlit","whisper"]
 )
